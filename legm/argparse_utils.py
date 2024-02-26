5ce69a1c95b243cb3130ae5b4e03164336f8c7a7
Georgios <georgioschochlakis@gmail.com>
2024-02-26 12:08:08 -0800

    Fix bug with parent's metadata

--- legm/argparse_utils.py
+++ legm/argparse_utils.py
@@ -41,14 +41,14 @@ def add_arguments(
 
 
 def add_metadata(
-    metadata: Dict[str, Dict[str, Any]],
+    metadata_dest: Dict[str, Dict[str, Any]],
     arguments: Dict[str, Dict[str, Any]],
     exclude: List[str] = None,
 ):
-    """Adds metadata from `arguments` to `metadata` in-place.
+    """Adds metadata from `arguments` to `metadata_dest` in-place.
 
     Args:
-        metadata: dict of metadata per parameter.
+        metadata_dest: dict of destination metadata per parameter.
         arguments: a dictionary with name of variable
             as key and kwargs as value.
         exclude: list of arguments to exclude.
@@ -60,43 +60,41 @@ def add_metadata(
 
     exclude = set(exclude or [])
     for k, v in arguments.items():
-        if k in exclude:
+        param_metadata = v.get("metadata", {})
+        if k in exclude or not param_metadata:
             continue
-        param_metadata = v.get("metadata", None)
-        if param_metadata:
-            k_has_splits = bool(v.get("splits", []))
-            parent = param_metadata.get("parent", None)
-            if parent:
-                parent_has_splits = bool(
-                    arguments[parent].get("metadata", {}).get("splits", [])
-                )
-            else:
-                parent_has_splits = False
 
-            assert (
-                k_has_splits or not parent_has_splits
-            ), "Parent of a parameter without splits cannot have splits."
+        k_has_splits = bool(v.get("splits", []))
+        parent = param_metadata.get("parent", None)
+        if parent:
+            parent_has_splits = bool(arguments[parent].get("splits", []))
+        else:
+            parent_has_splits = False
 
-            if not k_has_splits:
-                # if no splits, add metadata as is
-                metadata[k] = param_metadata
-            elif k_has_splits and not parent_has_splits:
-                # if splits and no parent splits,
-                # add metadata in split parameter
-                for split in v["splits"]:
-                    metadata[
-                        gridparse.GridArgumentParser._add_split_in_arg(k, split)
-                    ] = param_metadata
-            else:
-                # if splits and parent has splits,
-                # add metadata in split parameter with same-split parent
-                for split in v["splits"]:
-                    split_metadata = deepcopy(param_metadata)
-                    split_metadata["parent"] = (
-                        gridparse.GridArgumentParser._add_split_in_arg(
-                            parent, split
-                        )
+        assert (
+            k_has_splits or not parent_has_splits
+        ), "Parent of a parameter without splits cannot have splits."
+
+        if not k_has_splits:
+            # if no splits, add metadata as is
+            metadata_dest[k] = param_metadata
+        elif k_has_splits and not parent_has_splits:
+            # if splits and no parent splits,
+            # add metadata in split parameter
+            for split in v["splits"]:
+                metadata_dest[
+                    gridparse.GridArgumentParser._add_split_in_arg(k, split)
+                ] = param_metadata
+        else:
+            # if splits and parent has splits,
+            # add metadata in split parameter with same-split parent
+            for split in v["splits"]:
+                split_metadata = deepcopy(param_metadata)
+                split_metadata["parent"] = (
+                    gridparse.GridArgumentParser._add_split_in_arg(
+                        parent, split
                     )
-                    metadata[
-                        gridparse.GridArgumentParser._add_split_in_arg(k, split)
-                    ] = split_metadata
+                )
+                metadata_dest[
+                    gridparse.GridArgumentParser._add_split_in_arg(k, split)
+                ] = split_metadata
