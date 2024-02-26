831858b84f39cb365be45013313f58b4d3c484ec
Georgios <georgioschochlakis@gmail.com>
2024-02-25 18:11:51 -0800

    Account for splits in parent variable in parser metadata

--- legm/argparse_utils.py
+++ legm/argparse_utils.py
@@ -45,27 +45,58 @@ def add_metadata(
     arguments: Dict[str, Dict[str, Any]],
     exclude: List[str] = None,
 ):
-    """Adds metadata from `arguments` to `metadata`.
+    """Adds metadata from `arguments` to `metadata` in-place.
 
     Args:
         metadata: dict of metadata per parameter.
         arguments: a dictionary with name of variable
             as key and kwargs as value.
         exclude: list of arguments to exclude.
+
+    Raises:
+        AssertionError: if a parameter doesn't have `splits`
+        and its parent does.
     """
 
     exclude = set(exclude or [])
     for k, v in arguments.items():
-        if k not in exclude:
-            param_metadata = v.get("metadata", None)
-            has_splits = bool(v.get("splits", []))
-            if param_metadata:
-                if not has_splits:
-                    metadata[k] = param_metadata
-                else:
-                    for split in v["splits"]:
-                        metadata[
-                            gridparse.GridArgumentParser._add_split_in_arg(
-                                k, split
-                            )
-                        ] = param_metadata
+        if k in exclude:
+            continue
+        param_metadata = v.get("metadata", None)
+        if param_metadata:
+            k_has_splits = bool(v.get("splits", []))
+            parent = param_metadata.get("parent", None)
+            if parent:
+                parent_has_splits = bool(
+                    arguments[parent].get("metadata", {}).get("splits", [])
+                )
+            else:
+                parent_has_splits = False
+
+            assert (
+                k_has_splits or not parent_has_splits
+            ), "Parent of a parameter without splits cannot have splits."
+
+            if not k_has_splits:
+                # if no splits, add metadata as is
+                metadata[k] = param_metadata
+            elif k_has_splits and not parent_has_splits:
+                # if splits and no parent splits,
+                # add metadata in split parameter
+                for split in v["splits"]:
+                    metadata[
+                        gridparse.GridArgumentParser._add_split_in_arg(k, split)
+                    ] = param_metadata
+            else:
+                # if splits and parent has splits,
+                # add metadata in split parameter with same-split parent
+                for split in v["splits"]:
+                    split_metadata = deepcopy(param_metadata)
+                    split_metadata["parent"] = (
+                        gridparse.GridArgumentParser._add_split_in_arg(
+                            parent, split
+                        )
+                    )
+                    metadata[
+                        gridparse.GridArgumentParser._add_split_in_arg(k, split)
+                    ] = split_metadata
